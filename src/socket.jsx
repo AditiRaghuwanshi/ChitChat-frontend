b59a9613ce034bf80c6352ccd17beedb0dc5f524
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2025-05-20 17:03:52 -0700

    cors issues

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,26 +1,48 @@
-import io from "socket.io-client";
-import { createContext, useMemo, useContext } from "react";
-import { server } from "./constants/configure";
+// import io from "socket.io-client";
+// import { createContext, useMemo, useContext } from "react";
+// import { server } from "./constants/configure";
 
+
+// const SocketContext = createContext();
+
+// const getSocket = () =>  useContext(SocketContext);
+
+// const SocketProvider = ({ children }) => { 
+    
+// const socket = useMemo(() => io(server, {withCredentials: true }), []);
+
+//  return  (
+//     <SocketContext.Provider value={socket}>
+//       {children}
+//     </SocketContext.Provider>
+//   );
+
+// };  
+
+// export { SocketProvider, getSocket };
+
+
+
+
+import { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
+import { server } from "./constants/configure"; // Make sure this is without a trailing slash
 
 const SocketContext = createContext();
 
-const getSocket = () =>  useContext(SocketContext);
+export const getSocket = () => useContext(SocketContext);
 
-const SocketProvider = ({ children }) => { 
-    
-const socket = useMemo(() => io(server, {withCredentials: true }), []);
+export const SocketProvider = ({ children }) => {
+  const socket = useMemo(() => {
+    return io(server, {
+      withCredentials: true,
+      transports: ["websocket", "polling"], // 👈 add transports explicitly
+    });
+  }, []);
 
- return  (
+  return (
     <SocketContext.Provider value={socket}>
       {children}
     </SocketContext.Provider>
   );
-
-};  
-
-export { SocketProvider, getSocket };
-
-
-
-
+};
